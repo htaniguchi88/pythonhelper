f4f7d7339dc429dd541c23279c1f4d49b2f01a3d
htaniguchi88 <gucci.taniguchi88@gmail.com>
2019-03-15 20:59:06 +0900

    Set contemporary code

--- InterleaveInspector.py
+++ InterleaveInspector.py
@@ -1,7 +1,15 @@
 input = ['地上波', 'ダメ', '絶対']
+array = []
+j = 0
 
-def interleave(list):
-    for i in range(len(list)):
-        print(list[i])
+def interleave(arg_list):
+    for i in range(len(arg_list)):
+       perticular_list = list(arg_list[i])
+       if perticular_list[j] !== null
+        array.append(perticular_list[j])
+        j++
+
+### どこでjの上限値について命令するか
 
 interleave(input)
+print(array)
