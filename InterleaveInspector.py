0486f0fe93c9056ef7b5cdfbef06b8116df020d4
htaniguchi88 <gucci.taniguchi88@gmail.com>
2019-03-16 11:43:27 +0900

    要件通りに要素を取得

--- InterleaveInspector.py
+++ InterleaveInspector.py
@@ -5,11 +5,15 @@ j = 0
 def fetch_onecycle(arg_list):
     for i in range(len(arg_list)):
         target_list = list(arg_list[i])
-        print(target_list[j])
-
+        if j+1 <= len(target_list):
+            array.append(target_list[j])
+            print(target_list[j])
+        else:
+            pass
+            
 while j <= len(max((x for x in input), key=len)):
     fetch_onecycle(input)
     j += 1
-
+    
 print(array)
 
