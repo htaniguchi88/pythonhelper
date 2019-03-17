ec3b66d7298ff5b1cf6d8ff26e00864f7f72f614
htaniguchi88 <gucci.taniguchi88@gmail.com>
2019-03-18 08:56:00 +0900

    Remove comment from reverse

--- reverse.py
+++ reverse.py
@@ -1,6 +1,5 @@
 def reverse_all_characters(input):
   input_list = list(input)
- #for character in (reversed(str_list)):
   output_list = [str(i) for i in reversed(input)]
   output = ''.join(output_list)
 
