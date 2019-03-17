06261ae5984619a7ae70fd663ce7ed8b5ffe8dd5
htaniguchi88 <gucci.taniguchi88@gmail.com>
2019-03-17 21:54:10 +0900

    modify test

--- test_interleave.py
+++ test_interleave.py
@@ -7,3 +7,8 @@ class TestInterleave(unittest.TestCase):
   def test_interleave_all_characters(self):
     input = ['aa', 'bb', 'cc']
     self.assertEqual('abcabc', interleave.interleave_all_characters(input))
+
+
+  def test_interleave_all_characters(self):
+    input = ['a', 'bb', 'ccc']
+    self.assertEqual('abcbcc', interleave.interleave_all_characters(input))
