5a1c33a2a01efcfe61b3f434a758a25543b1386f
htaniguchi88 <gucci.taniguchi88@gmail.com>
2019-03-18 09:45:34 +0900

    flake8

--- test_reverse.py
+++ test_reverse.py
@@ -4,10 +4,10 @@ import reverse
 
 class TestReverse(unittest.TestCase):
 
-  def test_reverse_two_characters(self):
-    input = 'ab'
-    self.assertEqual('ba', reverse.reverse_all_characters(input))
+    def test_reverse_two_characters(self):
+        input = 'ab'
+        self.assertEqual('ba', reverse.reverse_all_characters(input))
 
-  def test_reverse_three_characters(self):
-    input = 'abc'
-    self.assertEqual('cba', reverse.reverse_all_characters(input))
+    def test_reverse_three_characters(self):
+        input = 'abc'
+        self.assertEqual('cba', reverse.reverse_all_characters(input))
