1dd7bcfa99addaea7e1ada4cf5d02bd9f78f6e7a
htaniguchi88 <gucci.taniguchi88@gmail.com>
2019-03-18 09:37:00 +0900

    flake8 test_integrate

--- test_integrate.py
+++ test_integrate.py
@@ -4,16 +4,14 @@ import integrate
 
 class TestIntegrate(unittest.TestCase):
 
-  def test_integrate_all_characters_same_characters_length_and_two_words(self):
-    input = ['a', 'b']
-    self.assertEqual('ab', integrate.integrate_words(input))
+    def test_integrate_words_same_characters_length_and_two_words(self):
+        input = ['a', 'b']
+        self.assertEqual('ab', integrate.integrate_words(input))
 
+    def test_integrate_words_same_characters_length_and_three_words(self):
+        input = ['a', 'b', 'c']
+        self.assertEqual('abc', integrate.integrate_words(input))
 
-  def test_integrate_all_characters_same_characters_length_and_three_words(self):
-    input = ['a', 'b', 'c']
-    self.assertEqual('abc', integrate.integrate_words(input))
-
-
-  def test_interleave_all_characters_deferent_characters_length_and_three_words(self):
-    input = ['a', 'bb', 'ccc']
-    self.assertEqual('abbccc', integrate.integrate_words(input))
+    def test_integrate_words_deferent_characters_length_and_three_words(self):
+        input = ['a', 'bb', 'ccc']
+        self.assertEqual('abbccc', integrate.integrate_words(input))
